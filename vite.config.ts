bab68efe67673af889f331eecdc55075297b94a7
vwhb王海兵 <haibingwang@trip.com>
2023-06-13 15:47:42 +0800

    fix relation of package.json

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: apiKey ? apiKey : "http://127.0.0.1:8080",
+        target: apiKey ? apiKey : "http://10.5.153.1:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
